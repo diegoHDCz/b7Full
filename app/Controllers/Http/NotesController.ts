ab0a654786c9be868d325d3125cabb2a7ce60dfa
Diego Czajka <diego.czajka@gmail.com>
2022-06-12 22:36:23 -0300

    backend notes

---
+++ app/Controllers/Http/NotesController.ts
@@ -0,0 +1,38 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Note from 'App/Models/Note'
+
+export default class NotesController {
+  public async index({}: HttpContextContract) {
+    const notes = await Note.all()
+
+    return notes
+  }
+
+  public async store({ request }: HttpContextContract) {
+    const data = request.only(['title', 'content', 'settingsbg', 'done'])
+
+    const note = await Note.create(data)
+    return note
+  }
+
+  public async show({ params }: HttpContextContract) {
+    const note = await Note.findOrFail(params.id)
+
+    return note
+  }
+
+  public async update({ request, params }: HttpContextContract) {
+    const note = await Note.findOrFail(params.id)
+    const data = request.only(['title', 'content', 'settingsbg', 'done'])
+
+    note.merge(data)
+
+    await note.save()
+  }
+
+  public async destroy({ params }: HttpContextContract) {
+    const note = await Note.findOrFail(params.id)
+
+    await note.delete()
+  }
+}
---
+++ start/routes.ts
@@ -0,0 +1,6 @@
+import Route from '@ioc:Adonis/Core/Route'
+
+Route.get('/', async () => {
+  return { hello: 'world' }
+})
+Route.resource('/notes', 'NotesController').apiOnly()
